f13e6d07d990fa6053c9e6325dd9fb2f9c1a3af9
PedroHRPBS <pedrohenriquerpbs@gmail.com>
2018-09-04 10:32:34 -0300

    feat: Add velocity tracking to trajectory

--- config/traj/MakePeriodicTrajectory.py
+++ config/traj/MakePeriodicTrajectory.py
@@ -12,31 +12,28 @@ phase=[0,0,0]
 amp = [1,0.4,.5]
 center = [0, 0, -2]
 
-with open('FigureEight.txt', 'w') as the_file:
-    t=0;
-    px = 0;
-    py = 0;
-    pz = 0;
+with open('FigureEightFFnew.txt', 'w') as the_file:
+    t=0
+    pastX = 0
+    pastY = 0
+    pastZ = 0
     while t <= maxtime:
-        x = math.sin(t * 2 * math.pi / period[0] + phase[0]) * radius * amp[0] + center[0];
-        y = math.sin(t * 2 * math.pi / period[1] + phase[1]) * radius * amp[1] + center[1];
-        z = math.sin(t * 2 * math.pi / period[2] + phase[2]) * radius * amp[2] + center[2];
-        the_file.write(fmt(t) + "," + fmt(x) + "," + fmt(y) + "," + fmt(z));
-		vx = 0;
-		vy = 0;
-		vz = 0;
-		######## BEGIN STUDENT CODE
-		
-		######## END STUDENT CODE
-		the_file.write("," + fmt(vx) + "," + fmt(vy) + "," + fmt(vz));
-		######## EXAMPLE SOLUTION
-        #the_file.write("," + fmt((x-px)/timestep) + "," + fmt((y-py)/timestep) + "," + fmt((z-pz)/timestep));
-		#px = x;
-        #py = y;
-        #pz = z;
-		######## END EXAMPLE SOLUTION
-		
-        the_file.write("\n");
+        x = math.sin(t * 2 * math.pi / period[0] + phase[0]) * radius * amp[0] + center[0]
+        y = math.sin(t * 2 * math.pi / period[1] + phase[1]) * radius * amp[1] + center[1]
+        z = math.sin(t * 2 * math.pi / period[2] + phase[2]) * radius * amp[2] + center[2]
+        the_file.write(fmt(t) + "," + fmt(x) + "," + fmt(y) + "," + fmt(z))
+
+        vx = (x - pastX) / timestep
+        vy = (y - pastY) / timestep
+        vz = (z - pastZ) / timestep
+
+        the_file.write("," + fmt(vx) + "," + fmt(vy) + "," + fmt(vz))
+
+        pastX = x
+        pastY = y
+        pastZ = z
         
-        t += timestep;
+        the_file.write("\n")
+        
+        t += timestep
             
